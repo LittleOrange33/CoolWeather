d128518ee74c30d169196d58f332f482e6149e91
admin <1125362615@qq.com>
2019-03-22 08:59:20 +0800

    SecondTask

--- app/src/main/java/com/example/admin/helloworld/MainActivity.java
+++ app/src/main/java/com/example/admin/helloworld/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.admin.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textView = (TextView) findViewById(R.id.getweather);
+        String weatherId = "CN101020200";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=e3f1bbd1c57e4560a8b22478df974f25";
+
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(responseText);
+                    }
+                });
+                textView.setText(responseText);
+            }
+
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                   e.printStackTrace();
+            }
+        });
     }
 }

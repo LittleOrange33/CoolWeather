506426403e79d151f5dd1ee3e5aa47c26377f219
admin <1125362615@qq.com>
2019-04-10 09:12:32 +0800

    FourthTask

--- app/src/main/java/com/example/admin/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/admin/helloworld/Main2Activity.java
@@ -1,39 +1,25 @@
 package com.example.admin.helloworld;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity {
     private TextView textView;
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.textView = (TextView) findViewById(R.id.getweather);
-        this.button = (Button) findViewById(R.id.button);
-        this.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
-            }
-        });
-        String weatherId = "CN101020200";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=e3f1bbd1c57e4560a8b22478df974f25";
-
-        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+        setContentView(R.layout.activity_main2);
+        this.textView = (TextView) findViewById(R.id.getmessage);
+//        String weatherId = "CN101020200";
+//        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=e3f1bbd1c57e4560a8b22478df974f25";
+          String CityUrl =  "http://guolin.tech/api/china";
+          HttpUtil.sendOkHttpRequest(CityUrl, new Callback() {
             @Override
             public void onResponse(okhttp3.Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
@@ -41,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         textView.setText(responseText);
-                }
+                    }
                 });
 //                textView.setText(responseText);
             }
 
             @Override
             public void onFailure(okhttp3.Call call, IOException e) {
-                   e.printStackTrace();
+                e.printStackTrace();
             }
         });
     }
